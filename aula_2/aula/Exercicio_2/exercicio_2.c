8dede642c474f6ae3b4449ddabafb4b78c526e51
ViniNovack <vininomn@gmail.com>
2026-08-20 12:02:08 -0300

    ataulização

--- aula_2/aula/Exercicio_2/exercicio_2.c
+++ aula_2/aula/Exercicio_2/exercicio_2.c
@@ -2,32 +2,8 @@
 #include <stdbool.h>
 
 int main(){
-    int x;
-    int y;
-    int z;
-    int a;
+    char x, y, z, temp;
+    printf("Digite ")
 
-    for(int i = 1, j = 4; i < j; i++, j--){
-        printf("Digite uma letra: ");
-        scanf("%d", &a);
-        switch(i){
-            case 1:
-                a = x;
-                break;
-            case 2:
-                a = y;
-                break;
-            case 3:
-                a = z;
-                break;
-        }
-    }
-
-    if(x > y && x > z){
-        a = x
-    } else if{
-        ;
-    }
-    printf("%d, %d, %d", );
     return 0;
 }
