0731487758483780f844000409cc182c74855078
ViniNovack <vininomn@gmail.com>
2026-08-22 17:29:01 -0300

    Atualização

--- Formativas/Formativa_2/formativa_2.c
+++ Formativas/Formativa_2/formativa_2.c
@@ -2,6 +2,29 @@
 #include <stdbool.h>
 
 int main() {
-    printf("Hello, wolrd!");
+    int t, d, m, y;
+    bool r = true;
+    printf("Digite os tres numeros: ");
+    scanf("%d %d %d", &t, &d, &m);
+
+    int i = 1;
+    while(i < (m + 1)){
+        printf("Digite o tempo %d: ", i);
+        scanf("%d", y);
+
+        if(t < y){
+            r = false;
+        }
+        
+
+        i++;
+    }
+
+    if(r == true){
+        printf("Y");
+    } else if(r == false){
+        printf("N");
+    }
+
     return 0;
 }
