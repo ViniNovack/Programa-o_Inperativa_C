20c30121ef12a36179873d84924c4fecdc8d199a
ViniNovack <vininomn@gmail.com>
2026-08-23 17:43:19 -0300

    termino_exercicio_2

--- aula_2/aula/Exercicio_2/exercicio_2.c
+++ aula_2/aula/Exercicio_2/exercicio_2.c
@@ -2,8 +2,29 @@
 #include <stdbool.h>
 
 int main(){
-    char x, y, z, temp;
-    printf("Digite ");
+    char x, y, z, aux;
+    printf("Digite tres letras, separadas por espaco: ");
+    scanf("%c %c %c", &x, &y, &z);
 
+    if(x > y){
+        aux = x;
+        x = y;
+        aux = y;
+
+    }
+
+    if(y > z){
+        aux = y;
+        y = z;
+        aux = z;
+    }
+
+    if(x > y){
+        aux = x;
+        x = y;
+        y = aux;
+    }
+    
+    printf("%c - %c - %c", x, y, z);
     return 0;
 }
