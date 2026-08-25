20e238858899484169a34062d26713eba2e8e30b
ViniNovack <vininomn@gmail.com>
2026-08-25 16:53:20 -0300

    Atualizacao

---
+++ aula_2/enum/enum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+enum Dia{SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO};
+
+int main() {
+    enum Dia dia_semana;
+    printf("Digite um dia da semana [0 a 6]: ");
+    scanf("%d", &dia_semana);
+    switch(dia_semana) {
+        case SEGUNDA: case TERCA: case QUARTA: case QUINTA: case SEXTA:
+            puts("trabalho");
+            break;
+        case SABADO:
+            puts("compras");
+            break;
+        case DOMINGO:
+            puts("descanso");
+            break;
+        default:
+            puts("ERRO!");
+            break;
+    }
+    return 0;
+}
